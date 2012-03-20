e0f39c274151d2236cd0c9e4193fac7552e8b256
Anno <anno.tao@gmail.com>
2012-03-20 16:14:09 +0100

    PlayerCharacter update

--- CodenameG/src/edu/chl/codenameg/model/entity/PlayerCharacter.java
+++ CodenameG/src/edu/chl/codenameg/model/entity/PlayerCharacter.java
@@ -13,11 +13,24 @@ public class PlayerCharacter {
 		hitbox = new Hitbox(5, 10);
 	}
 	
+	public PlayerCharacter(Point position) {
+		this();
+		this.position = position;
+	}
+	
 	public Point getPosition() {
 		return new Point(position);
 	}
 	
 	public Hitbox getHitBox() {
 		return new Hitbox(hitbox);
+	}
+	
+	public void jump() {
+		//TODO Complete this method
+	}
+	
+	public void move() {
+		//TODO Complete this method
 	}
 }
