6990dd71f9047a842486cfa71c0429389361b711
Simon Nilsson <sim.nilsson@gmail.com>
2012-03-20 22:39:10 +0100

    PlayerCharacter extends Entity

--- CodenameG/src/edu/chl/codenameg/model/entity/PlayerCharacter.java
+++ CodenameG/src/edu/chl/codenameg/model/entity/PlayerCharacter.java
@@ -2,9 +2,11 @@ package edu.chl.codenameg.model.entity;
 
 import java.awt.Point;
 
+import edu.chl.codenameg.model.Entity;
 import edu.chl.codenameg.model.Hitbox;
+import edu.chl.codenameg.model.Vector2D;
 
-public class PlayerCharacter {
+public class PlayerCharacter extends Entity{
 	private Point position;
 	private Hitbox hitbox;
 	
@@ -22,15 +24,34 @@ public class PlayerCharacter {
 		return new Point(position);
 	}
 	
-	public Hitbox getHitBox() {
-		return new Hitbox(hitbox);
-	}
-	
 	public void jump() {
 		//TODO Complete this method
 	}
 	
 	public void move() {
 		//TODO Complete this method
+	}
+
+	@Override
+	public Hitbox getHitbox() {
+		return new Hitbox(hitbox);
+	}
+
+	@Override
+	public Vector2D getVector2D() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isColliding() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void collide(Entity e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
