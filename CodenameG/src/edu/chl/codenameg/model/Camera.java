85a935c4a6b5892d04c48c12d9ae7d55af213572
Anno <anno.tao@gmail.com>
2012-03-20 15:00:26 +0100

    Fixade world, men huvudsak CameraTest och Camera implementation som passar testet

--- CodenameG/src/edu/chl/codenameg/model/Camera.java
+++ CodenameG/src/edu/chl/codenameg/model/Camera.java
@@ -1,19 +1,40 @@
 package edu.chl.codenameg.model;
 
+import java.awt.Dimension;
 import java.awt.Point;
-import java.awt.Rectangle;
 
 public class Camera {
+	private Point position;
+	private Dimension size;
+
+	
 	public Camera() {
-		
+		position = new Point(0,0);
+		size = new Dimension(1024, 768);
 	}
+	
+	public Camera(Dimension size) {
+		this();
+		this.size = size;
+	}
+	
+//	public Camera(Point position) {
+//		this();
+//		this.position = position;
+//	}
+//	
+//	public Camera(Point position, Dimension size) {
+//		this();
+//		this.position = position;
+//		this.size = size;
+//	}
 	
 	public Point getPosition() {
-		
+		return position;
 	}
 	
-	public Rectangle getSize() {
-		
+	public Dimension getSize() {
+		return size;
 	}
 	
 }
