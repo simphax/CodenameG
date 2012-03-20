2e979b7062b041c48dfff11a076a0469902313ab
Sebastian d'Aubigné <daubigne@student.chalmers.se>
2012-03-20 15:16:43 +0100

    Added entity again, attempt 1.

---
+++ CodenameG/src/edu/chl/codenameg/model/Entity.java
@@ -0,0 +1,26 @@
+package edu.chl.codenameg.model;
+
+import java.awt.Point;
+
+
+public abstract class Entity {
+	private Point pt;
+	
+	//methods to be tested
+	public void setPosition(Point p){
+		this.pt =p;
+	}
+	public Point getPosition(){
+		return this.pt;
+	}
+	public abstract Hitbox getHitbox();
+	
+	public abstract Vector2D getVector2D();
+	
+	public abstract boolean isColliding();
+
+	public void collide(Entity e) {
+		
+	}
+}
+
