03f65be798a5a90dcbde9d3cd9130ccebc698406
Simon Nilsson <sim.nilsson@gmail.com>
2012-03-21 22:20:53 +0100

    Working collision detection

--- CodenameG/src/edu/chl/codenameg/model/World.java
+++ CodenameG/src/edu/chl/codenameg/model/World.java
@@ -29,15 +29,13 @@ public class World {
 
 	public void update(int elapsedTime) {
 		for (Entity e : this.getEntities()) {
+
 			Point temp = e.getPosition();
 			Vector2D vector = e.getVector2D();
-			if(vector != null) {
-				e.setPosition(new Point((int)e.getPosition().getX()+e.getVector2D().getX(),(int)e.getPosition().getY()+e.getVector2D().getY()));
+			if (vector != null && (vector.getX() > 0 || vector.getY() > 0)) {
+				e.setPosition(new Point((int) e.getPosition().getX() + e.getVector2D().getX(), (int) e.getPosition().getY() + e.getVector2D().getY()));
 			}
 			this.checkCollision(e);
-			if(e.isColliding()) {
-				e.setPosition(temp);
-			}
 			e.update(elapsedTime);
 		}
 	}
@@ -46,18 +44,37 @@ public class World {
 		for (Entity target : this.getEntities()) {
 			if (target != e) {
 				boolean collision = false;
-				
-				Rectangle thisRectangle = new Rectangle(e.getPosition(),new Dimension(e.getHitbox().getWidth(),e.getHitbox().getHeight()));
-				Rectangle targetRectangle = new Rectangle(target.getPosition(),new Dimension(target.getHitbox().getWidth(),target.getHitbox().getHeight()));
-				
+
+				Rectangle thisRectangle = new Rectangle(e.getPosition(), new Dimension(e.getHitbox().getWidth(), e.getHitbox().getHeight()));
+				Rectangle targetRectangle = new Rectangle(target.getPosition(), new Dimension(target.getHitbox().getWidth(), target.getHitbox().getHeight()));
 				if (thisRectangle.intersects(targetRectangle)) {
 					collision = true;
+
+					Rectangle intersection = thisRectangle.intersection(targetRectangle);
+
+					//Collision on bottom
+					if (intersection.y == targetRectangle.y) {
+						e.setPosition(new Point(e.getPosition().x, e.getPosition().y - intersection.height));
+					}
+					//Collision on top
+					if (intersection.y + intersection.height == targetRectangle.y) {
+						e.setPosition(new Point(e.getPosition().x, e.getPosition().y + intersection.height));
+					}
+					//Collision on right
+					if (intersection.x == targetRectangle.x) {
+						e.setPosition(new Point(e.getPosition().x - intersection.width, e.getPosition().y));
+					}
+					//Collision on left
+					if (intersection.x + intersection.width == targetRectangle.x) {
+						e.setPosition(new Point(e.getPosition().x + intersection.width, e.getPosition().y));
+					}
 				}
 				if (collision) {
 					e.collide(target);
 					target.collide(e);
 				}
 			}
+
 		}
 	}
 
