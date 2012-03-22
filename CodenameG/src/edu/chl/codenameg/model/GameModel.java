1151623c92f5337cfff147c563c36733fec4ff37
Simon Nilsson <sim.nilsson@gmail.com>
2012-03-22 15:58:11 +0100

    Added default world in GameModel

--- CodenameG/src/edu/chl/codenameg/model/GameModel.java
+++ CodenameG/src/edu/chl/codenameg/model/GameModel.java
@@ -1,12 +1,30 @@
 package edu.chl.codenameg.model;
 
+import edu.chl.codenameg.model.entity.Block;
+import edu.chl.codenameg.model.entity.PlayerCharacter;
+
 public class GameModel {
 	
 	private World world;
 	private boolean running;
 	
 	public GameModel() {
+		
+		//TODO Default world?
 		this.world = new World();
+		
+		Block block = new Block();
+		block.setPosition(new Position(100,200));
+		block.setHitbox(new Hitbox(200,20));
+		
+		PlayerCharacter pc = new PlayerCharacter();
+		pc.setPosition(new Position(200,50));
+		
+		world.add(block);
+		world.add(pc);
+		
+		this.setWorld(world);
+		this.startGame();
 	}
 	
 	public void setWorld(World w) {
