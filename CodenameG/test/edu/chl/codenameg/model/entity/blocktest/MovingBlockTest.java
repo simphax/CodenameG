05c216f6ea730c21d2c4f861a3e21e1461354d59
Sebastian d'Aubigné <daubigne@student.chalmers.se>
2012-03-27 16:19:41 +0200

    Just a backup. testMoving in MovingBlockTest needs to be rewrit to check for the vector result.

--- CodenameG/test/edu/chl/codenameg/model/entity/blocktest/MovingBlockTest.java
+++ CodenameG/test/edu/chl/codenameg/model/entity/blocktest/MovingBlockTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.chl.codenameg.model.Position;
 import edu.chl.codenameg.model.Vector2D;
 import edu.chl.codenameg.model.entity.MovingBlock;
 import edu.chl.codenameg.model.entity.PlayerCharacter;
@@ -26,5 +27,14 @@ public class MovingBlockTest {
 		pc.collide(mb);
 		mb.collide(pc);
 		assertTrue(mb.isColliding() && pc.isColliding() && pc.getVector2D().equals(mb.getVector2D()));
+	}
+	@Test
+	public void testMoving(){
+		MovingBlock mb = new MovingBlock();
+		Position p = mb.getPosition();
+		System.out.println(""+p.getX()+ " "+p.getY()+"\n"+mb.getPosition().getX() + " " + mb.getPosition().getY());
+		mb.update(100);
+		System.out.println(""+mb.getPosition().getX() + " " + mb.getPosition().getY());
+		assertTrue(p.equals(new Position(2.5f,2.5f)) && mb.getPosition().equals(new Position(7.5f,7.5f)));
 	}
 }
