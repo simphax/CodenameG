5bf41cf9975e6412aee2aa43ab86582dca3db148
CaptainTec <alex.malmek@hotmail.com>
2012-03-28 13:54:35 +0200

    CollisionEvent done

--- CodenameG/src/edu/chl/codenameg/model/CollisionEvent.java
+++ CodenameG/src/edu/chl/codenameg/model/CollisionEvent.java
@@ -1,19 +1,20 @@
 package edu.chl.codenameg.model;
 
 public class CollisionEvent {
-
+	
+	Entity e;
+	Direction d;
 	
 	public CollisionEvent(Entity e, Direction d) {
-		
+		this.e = e;
+		this.d = d;
 	}
 	
 	public Entity getEntity() {
-		
+		return e;
 	}
-	public Entity getDirection() {
-		
+	public Direction getDirection() {
+		return d;
 	}
-	
-	
 	
 }
