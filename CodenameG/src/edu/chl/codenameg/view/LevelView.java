76f676e0e861638698e109fdf472b3bfe4cdc36f
CaptainTec <alex.malmek@hotmail.com>
2012-04-25 14:50:29 +0200

    Implemented LethalBlock graphics

--- CodenameG/src/edu/chl/codenameg/view/LevelView.java
+++ CodenameG/src/edu/chl/codenameg/view/LevelView.java
@@ -6,10 +6,12 @@ import org.newdawn.slick.*;
 import edu.chl.codenameg.model.Entity;
 import edu.chl.codenameg.model.GameModel;
 import edu.chl.codenameg.model.entity.Block;
+import edu.chl.codenameg.model.entity.LethalBlock;
 import edu.chl.codenameg.model.entity.LethalMovingBlock;
 import edu.chl.codenameg.model.entity.PlayerCharacter;
 import edu.chl.codenameg.view.entity.BasicEntityView;
 import edu.chl.codenameg.view.entity.BlockView;
+import edu.chl.codenameg.view.entity.LethalBlockView;
 import edu.chl.codenameg.view.entity.LethalMovingBlockView;
 import edu.chl.codenameg.view.entity.PlayerCharacterView;
 
@@ -39,7 +41,11 @@ public class LevelView {
 					EntityView view = new LethalMovingBlockView();
 					view.render(e, g);
 					entityMap.put(e, view);
-				} else if (e instanceof Block){
+				}else if (e instanceof LethalBlock){
+					EntityView view = new LethalBlockView();
+					view.render(e, g);
+					entityMap.put(e, view);
+				}  else if (e instanceof Block){
 					EntityView view = new BlockView();
 					view.render(e, g);
 					entityMap.put(e, view);
---
+++ CodenameG/src/edu/chl/codenameg/view/entity/LethalBlockView.java
@@ -0,0 +1,37 @@
+package edu.chl.codenameg.view.entity;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
+import edu.chl.codenameg.model.Entity;
+import edu.chl.codenameg.model.entity.LethalBlock;
+import edu.chl.codenameg.view.EntityView;
+
+public class LethalBlockView implements EntityView{
+	private SpriteSheet spriteSheet = null;
+	private Animation spin = null;
+	public LethalBlockView(){
+		try {
+			spriteSheet = new SpriteSheet("res/Muncher.png", 32, 32,Color.white);
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		spin = new Animation();
+		for (int i = 0; i < 3; i++) {
+			spin.addFrame(spriteSheet.getSprite(i, 0), 200);
+		}
+	}
+
+@Override
+public void render(Entity ent, Graphics g) {
+	if (ent instanceof LethalBlock) {
+		LethalBlock lb = (LethalBlock) ent;
+		spin.draw(lb.getPosition().getX(), lb.getPosition().getY());
+	}
+	
+}
+}
