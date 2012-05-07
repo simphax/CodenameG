66574136e7175cfcb3b9a17004696da10e8840a8
Simon Nilsson <sim.nilsson@gmail.com>
2012-05-07 15:23:49 +0200

    Removed unused imports

--- CodenameG/src/edu/chl/codenameg/main/GameApp.java
+++ CodenameG/src/edu/chl/codenameg/main/GameApp.java
@@ -1,33 +1,13 @@
 package edu.chl.codenameg.main;
 
-import java.awt.Point;
-
-import javax.swing.JFrame;
-
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
 import edu.chl.codenameg.controller.GameController;
-import edu.chl.codenameg.model.Entity;
-import edu.chl.codenameg.model.GameModel;
-import edu.chl.codenameg.model.Hitbox;
-import edu.chl.codenameg.model.Position;
-import edu.chl.codenameg.model.World;
-import edu.chl.codenameg.model.entity.Block;
-import edu.chl.codenameg.model.entity.PlayerCharacter;
-import edu.chl.codenameg.view.GameView;
-import edu.chl.codenameg.view.LevelView;
-import edu.chl.codenameg.view.MainView;
 
 public class GameApp {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		
-//		GameModel model = new GameModel();
-		//GameView view = new MainView(model);
 		AppGameContainer agc = null;
 		GameController controller = new GameController();
 		
@@ -36,7 +16,6 @@ public class GameApp {
 			agc.setTargetFrameRate(60);
 			agc.start();
 		} catch (SlickException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
