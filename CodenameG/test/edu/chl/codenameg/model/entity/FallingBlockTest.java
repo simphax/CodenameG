1e1595062b0c3a49b42ae63a6fd6f04fb13def8e
Sebastian d'Aubigné <daubigne@student.chalmers.se>
2012-05-19 12:15:57 +0200

    Added FallingBlock test, renamed Demo level and continued its creation.

--- CodenameG/test/edu/chl/codenameg/model/entity/FallingBlockTest.java
+++ CodenameG/test/edu/chl/codenameg/model/entity/FallingBlockTest.java
@@ -4,51 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.chl.codenameg.model.CollisionEvent;
+import edu.chl.codenameg.model.Direction;
+import edu.chl.codenameg.model.Vector2D;
+import edu.chl.codenameg.model.World;
+
 public class FallingBlockTest {
 
 	@Test
 	public void testCollide() {
-		fail("Not yet implemented");
+		PlayerCharacter gnarp = new PlayerCharacter(new World());
+		FallingBlock fb = new FallingBlock();
+		CollisionEvent evt1 = new CollisionEvent(gnarp,Direction.BOTTOM);
+		fb.collide(evt1);
+		Vector2D v2d1 = new Vector2D(fb.getVector2D());
+		CollisionEvent evt2 = new CollisionEvent(new MovableBlock(),Direction.TOP);
+		fb.collide(evt2);
+		Vector2D v2d2 = new Vector2D(fb.getVector2D());
+		CollisionEvent evt3 = new CollisionEvent(gnarp,Direction.TOP);
+		fb.collide(evt3);
+		Vector2D v2d3 = new Vector2D(fb.getVector2D());
+		assertTrue(v2d1.equals(new Vector2D(0,0)) && v2d2.equals(v2d1) && v2d3.equals(new Vector2D(0,2)));
+		
+		
 	}
 
-	@Test
-	public void testSetPosition() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	public void testSetVector2D() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCollideTypes() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetType() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddVector2D() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFallingBlock() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFallingBlockPosition() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFallingBlockPositionHitbox() {
-		fail("Not yet implemented");
-	}
 
 }
