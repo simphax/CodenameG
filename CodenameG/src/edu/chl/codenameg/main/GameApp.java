1930671ff6d3692224ecf39e613cf244da3d9155
Anno <anno.tao@gmail.com>
2012-05-20 18:12:32 +0200

    Added documentation and comments for edu.chl.codenameg.main

--- CodenameG/src/edu/chl/codenameg/main/GameApp.java
+++ CodenameG/src/edu/chl/codenameg/main/GameApp.java
@@ -5,6 +5,11 @@ import org.newdawn.slick.SlickException;
 
 import edu.chl.codenameg.controller.GameController;
 
+/**
+ * This is the runnable main class that starts Romijam
+ * @author ???
+ *
+ */
 public class GameApp {
 
 	public static void main(String[] args) {
@@ -12,6 +17,7 @@ public class GameApp {
 		GameController controller = new GameController();
 		
 		try {
+			// Sets the settings of the game and then starts it
 			agc = new AppGameContainer(controller,500,500,false);
 			agc.setTargetFrameRate(60);
 			agc.start();
@@ -19,7 +25,4 @@ public class GameApp {
 			e.printStackTrace();
 		}
 	}
-	
-	
-
 }
