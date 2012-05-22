c428f855f5c83b6f016b230dff3cc01f7fe59f83
Anno <anno.tao@gmail.com>
2012-05-22 09:41:54 +0200

    Added waterTest

--- CodenameG/test/edu/chl/codenameg/model/entity/WaterTest.java
+++ CodenameG/test/edu/chl/codenameg/model/entity/WaterTest.java
@@ -1,84 +1,21 @@
 package edu.chl.codenameg.model.entity;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import edu.chl.codenameg.model.CollisionEvent;
+import edu.chl.codenameg.model.Direction;
 
 public class WaterTest {
 
 	@Test
-	public void testWater() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWaterPosition() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWaterPositionHitbox() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetHitbox() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetHitbox() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetVector2D() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIsColliding() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testCollide() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetPosition() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUpdate() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetPosition() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetVector2D() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCollideTypes() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetType() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddVector2D() {
-		fail("Not yet implemented");
+		Water water = new Water();
+		Block b = new Block();
+		CollisionEvent ce = new CollisionEvent(b, Direction.TOP);
+		water.collide(ce);
+		assertTrue(water.isColliding());
 	}
 
 }
